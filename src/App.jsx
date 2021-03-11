71231cc310c07482cf72a53baf46916c21499501
chriskang24 <christopherjkang24@gmail.com>
2021-03-11 18:40:35 -0500

    add react routes to login/register && states

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { useState } from 'react'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -7,15 +14,29 @@ import Register from './components/Register';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
+    <Router>
+      <div className="App">
+        <h1>Advanced Movie Search App:</h1>
+        <Switch>
+          <Route path="/" exact >
+            <Home />
+          </Route>
 
-    <div className="App">
-      <Home />
-      <Login />
-      <Register />
-    </div>
+          <Route path="/login">
+            <Login setUser={setUser}/>
+          </Route>
 
+          <Route path="/register">
+            <Register setUser={setUser}/>
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
-}
+} 
 
 export default App;
