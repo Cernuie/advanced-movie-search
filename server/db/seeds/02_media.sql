2d0b0d614f12edfddb70a2fc2a072f7e9f86d2f1
chriskang24 <christopherjkang24@gmail.com>
2021-03-12 17:34:39 -0500

    add initial database schema & seeds

---
+++ server/db/seeds/02_media.sql
@@ -0,0 +1,7 @@
+INSERT into media (title, type, year, imdb_id, tmdb_id)
+VALUES
+('Game of Thrones', 'tv', 2011,'tt0944947', 1399),
+('Godzilla vs. Kong', 'movie', 2021, 'tt5034838', 399566),
+('Ed Wood', 'movie', 1994, 'tt0109707', 522)
+
+
