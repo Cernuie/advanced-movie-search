dcd19c1d7be32ba1d3ef01b66d32947f7b425496
chriskang24 <christopherjkang24@gmail.com>
2021-03-13 18:20:40 -0500

    add axios calls to test search to API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,64 @@
-import { useState } from 'react'
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
-
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import Header from "./components/Header";
+import Movie from "./components/Movie";
+import Search from "./components/Search";
 
-function App() {
-  const [user, setUser] = useState(null)
+const axios = require('axios');
+
+const App = () => {
+
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  let url = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
+
+  useEffect(() => {
+    axios.get(url)
+      .then((response) => {
+        setMovies(response.data.Search)
+        setLoading(false)
+      });
+  }, []);
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+  axios.get(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+    .then((response) => {
+      console.log(response.data)
+      if (response.data.Response === "True") {
+   
+        setMovies(response.data.Search)
+        setLoading(false)
+      }
+    })
+    .catch((error) => console.error(`Error: ${error}`))
+
+  }
+
 
   return (
-    <Router>
-      <div className="App">
-        <h1>Advanced Movie Search App:</h1>
-        <Switch>
-          <Route path="/" exact >
-            <Home />
-          </Route>
-
-          <Route path="/login">
-            <Login setUser={setUser}/>
-          </Route>
-
-          <Route path="/register">
-            <Register setUser={setUser}/>
-          </Route>
-
-        </Switch>
+    <div className="App">
+      <Header text="Advanced Movie Search" />
+      <Search search={search} />
+      <p className="App-intro"> Favourite movies here</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <span>loading... </span> 
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.title}`} movie={movie} />
+          ))
+        )}
       </div>
-    </Router>
+    </div>
+
   );
-} 
+}
 
 export default App;
