0c43df1aabb1ca734991d5f5160cd6784bd90342
Alexander Nguyen <cernvii@gmail.com>
2021-03-14 15:31:53 -0400

    partial merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,57 +1,20 @@
-<<<<<<< HEAD
 import React from 'react';
 import { useState } from 'react'
-=======
-import React, { useState, useEffect } from 'react'
-import './App.css';
-import Header from "./components/Header";
-import Movie from "./components/Movie";
-import Search from "./components/Search";
->>>>>>> origin/movie-search-test-framework
 
-const axios = require('axios');
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-<<<<<<< HEAD
 import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login'
 
-=======
-const App = () => {
->>>>>>> origin/movie-search-test-framework
 
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
+import './App.css';
 
-  let url = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
-
-  useEffect(() => {
-    axios.get(url)
-      .then((response) => {
-        setMovies(response.data.Search)
-        setLoading(false)
-      });
-  }, []);
-
-  const search = searchValue => {
-    setLoading(true);
-    setErrorMessage(null);
-
-  axios.get(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
-    .then((response) => {
-      console.log(response.data)
-      if (response.data.Response === "True") {
-   
-        setMovies(response.data.Search)
-        setLoading(false)
-      }
-    })
-    .catch((error) => console.error(`Error: ${error}`))
-
-  }
-
-<<<<<<< HEAD
 function App() {
   const [token, setToken] = useState();
 
@@ -85,27 +48,8 @@ function App() {
             <Home />
           </Route>
         </Switch>
-=======
-
-  return (
-    <div className="App">
-      <Header text="Advanced Movie Search" />
-      <Search search={search} />
-      <p className="App-intro"> Favourite movies here</p>
-      <div className="movies">
-        {loading && !errorMessage ? (
-          <span>loading... </span> 
-        ) : errorMessage ? (
-          <div className="errorMessage">{errorMessage}</div>
-        ) : (
-          movies.map((movie, index) => (
-            <Movie key={`${index}-${movie.title}`} movie={movie} />
-          ))
-        )}
->>>>>>> origin/movie-search-test-framework
       </div>
-    </div>
-
+    </Router>
   );
 }
 
