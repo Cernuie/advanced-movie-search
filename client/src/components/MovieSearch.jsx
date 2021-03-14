cd053489bb0b226b1e893950a1adb46d0f4338e5
chriskang24 <christopherjkang24@gmail.com>
2021-03-14 14:22:33 -0400

    add props to extract api data for movie search

---
+++ client/src/components/MovieSearch.jsx
@@ -0,0 +1,32 @@
+import React, { Fragment, useState, useEffect } from "react";
+// import axios from "axios";
+
+import SearchBar from "./SearchBar";
+import Results from "./Results"
+const axios = require('axios');
+
+export default function MovieSearch() {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = `https://www.omdbapi.com/?s=${term}&apikey=4a3b711b`
+    
+    axios.get(apiUrl)
+      .then((response) => {
+        console.log(response.data )
+        console.log(response.data.Search);
+        setResults([...response.data.Search]);
+      })
+      .catch((e) => console.log(`error ${e}`));
+  }, [term]);
+
+  return (
+    <Fragment>
+      <div>
+        <SearchBar onSearch={term => setTerm(term)} />
+        <Results results={results} />
+      </div>
+    </Fragment>
+  );
+}
