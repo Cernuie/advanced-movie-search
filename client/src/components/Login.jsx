c1ff39a531053d5069952368d2a275385d417c1c
Alexander Nguyen <cernvii@gmail.com>
2021-03-14 00:56:54 +0000

    login fixes, starting register functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,38 +2,64 @@ import React from 'react';
 import axios from 'axios'
 import { useState } from 'react'
 import "./Login.scss"
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 
-export default function Login() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [token, setToken] = useState();
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-  return (
-    <div className="Login">
-      <Form >
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit">
-          Login
-        </Button>
-      </Form>
-    </div>
+  const handleSubmit = (event) => {
+    axios.post(
+      "http://localhost:8080/login",
+      {
+      email: email,
+      password: password
+      },
+    )
+    .then(res => {
+      if (res.data) {
+        localStorage.setItem("token", JSON.stringify(res.data.token));
+        props.setToken(res.data.token);
+      }
+      return res.data;
+    })
+  }
+
+  const validateForm = () => {
+    if (!email) {
+      setMessage("Email cannot be blank");
+      return;
+    } else if (!password) {
+      setMessage("Password cannot be blank");
+      return;
+    } else {
+      setMessage("");
+      handleSubmit(email, password)
+      .catch(err => setMessage(err.response.data.error));
+    }
+  }
+
+  return(
+    <section className="page">
+      <div className="login-container">
+      <div id="loginPage">
+        <header className="login-header">Login</header>
+        
+        {message && <div className="alert alert-danger">{message}</div>}
+        
+        <form className="form-container" onSubmit={event => event.preventDefault()}>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" placeholder="Enter email" value={email} onChange={event => setEmail(event.target.value)}/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)}/>
+          </div>
+          <button type="button" className="btn btn-primary" onClick={validateForm}>Submit</button>
+        </form>
+      </div>
+      </div>
+    </section>
   );
   }
