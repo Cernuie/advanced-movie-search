160d8428a0ee37237192da6baad6e383bc8157e7
Alexander Nguyen <cernvii@gmail.com>
2021-03-17 12:56:50 -0400

    filling out favourites

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,22 +9,22 @@ import Register from "./components/Register";
 
 function App() {
   const [user, setUser] = useState("")
-
+  const [token, setToken] = useState("")
   return (
 
     <Router>
       <div>
         <h1 className="centre">Advanced Movie Search App:</h1>
-        <NavBar {... {user, setUser}} />
+        <NavBar {... {user,token, setToken, setUser}} />
         <Switch>
           <Route path="/movie/:id">
             <MovieOverview />
           </Route>
           <Route path="/login">
-            <Login {... {setUser}} />
+            <Login {... {setUser, setToken}} />
           </Route>
           <Route path="/register">
-            <Register {... {setUser}}/>
+            <Register {... {setUser, setToken}}/>
           </Route>
 
           <Route path="/" exact>
