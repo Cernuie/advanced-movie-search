7473d11c66e0c5e5d6f41359f7d2dab41c749498
chriskang24 <christopherjkang24@gmail.com>
2021-03-22 15:13:43 -0400

    outline changes

--- server/db/seeds/03_reviews.sql
+++ server/db/seeds/03_reviews.sql
@@ -1,10 +1,10 @@
 INSERT into reviews (user_id, imdb_id, user_review, user_rating)
 VALUES
 (1, 'tt0111161', 'Stephan loves the avengers movie', 7),
-(2, 'tt5109280', 'Alex loves game of thrones series', 6),
-(2, 'tt1361336', 'Stephan LOVES THIS MOVIE', 5),
-(3, 'tt5109280', 'Stephan LOVES Raya and the dragons!', 10),
-(3, 'tt12361974', 'Alex gives this movie a bad rating!', 1)
+(2, 'tt5109280', 'Alex loves game of thrones series!', 6),
+(2, 'tt1361336', 'Stephan thought this movie was meh', 5),
+(3, 'tt5109280', 'Stephan LOVES Raya and the Dragons!', 10),
+(3, 'tt12361974', 'Alex hates this movie! It was so bad!', 1)
 
 
 
